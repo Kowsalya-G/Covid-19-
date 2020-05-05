17d5fd0ca89d6a2293dd180489bce6e4db1498b4
Kowsalya G <Kowsalya-6100@csez.zohocorpin.com>
2020-05-05 17:51:58 +0530

    Updates

--- precache-manifest.bdb445a008e60fac6564b0c2d0f56f13.js
+++ precache-manifest.bdb445a008e60fac6564b0c2d0f56f13.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bb4f80ee71d857e88542accb6540b1dc",
+    "revision": "c64697d800ef7046d6962f85b6a9074a",
     "url": "/Covid-19-/index.html"
   },
   {
-    "revision": "a516b8db62a6fa72a9cd",
+    "revision": "21ade47a457f7847758c",
     "url": "/Covid-19-/static/css/2.52e704ef.chunk.css"
   },
   {
-    "revision": "44fb73703ae285445b19",
+    "revision": "ea1a2e3c16337107580e",
     "url": "/Covid-19-/static/css/main.522a1777.chunk.css"
   },
   {
-    "revision": "a516b8db62a6fa72a9cd",
-    "url": "/Covid-19-/static/js/2.6464a0cb.chunk.js"
+    "revision": "21ade47a457f7847758c",
+    "url": "/Covid-19-/static/js/2.61930b0d.chunk.js"
   },
   {
     "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
-    "url": "/Covid-19-/static/js/2.6464a0cb.chunk.js.LICENSE.txt"
+    "url": "/Covid-19-/static/js/2.61930b0d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "44fb73703ae285445b19",
-    "url": "/Covid-19-/static/js/main.927beed1.chunk.js"
+    "revision": "ea1a2e3c16337107580e",
+    "url": "/Covid-19-/static/js/main.af99224f.chunk.js"
   },
   {
     "revision": "b14df98467215ad6a782",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Covid-19-/precache-manifest.bb6e2b2d8ec82236af9e52e70247f678.js"
+  "/Covid-19-/precache-manifest.bdb445a008e60fac6564b0c2d0f56f13.js"
 );
 
 self.addEventListener('message', (event) => {
